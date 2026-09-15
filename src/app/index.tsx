c665cea9d77efd8340bcacc1b5e83f9dd9b22b8f
MScarrott <scarrottmatthew@gmail.com>
2026-09-15 14:26:44 -0600

    Removed 'title' value in button tag

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import { Alert, Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <>
       <View style={styles.container}>
         <Text>Edit src/app/index.tsx to edit this screen.</Text>
-        <Button onPress={() => Alert.alert("Alert Button pressed.")} title="Alert"/>
+        <Button onPress={() => Alert.alert("Alert Button pressed.")} />
       </View>
     </>
   );
