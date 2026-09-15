0da7df19654427b4dc13be248437fabdc483e7b2
MScarrott <scarrottmatthew@gmail.com>
2026-09-15 14:13:17 -0600

    Added a button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>Edit src/app/index.tsx to edit this screen.</Text>
+        <Button onPress={() => console.log("Alert Button pressed.")} title="Alert"/>
+      </View>
+    </>
   );
 }
 
