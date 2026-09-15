af117043fbb456025de9202297ac4492915fb529
MScarrott <scarrottmatthew@gmail.com>
2026-09-15 14:25:59 -0600

    Updated button tag
    console.log --> Alert.alert

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
 
 export default function Index() {
   return (
     <>
       <View style={styles.container}>
         <Text>Edit src/app/index.tsx to edit this screen.</Text>
-        <Button onPress={() => console.log("Alert Button pressed.")} title="Alert"/>
+        <Button onPress={() => Alert.alert("Alert Button pressed.")} title="Alert"/>
       </View>
     </>
   );
