9d8ca7f091c314eecfe95d409291f8ae791be17a
MScarrott <scarrottmatthew@gmail.com>
2026-09-15 14:27:37 -0600

    Readded 'title' value to button tag

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,11 @@
-import { Alert, Button, StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
 
 export default function Index() {
   return (
     <>
       <View style={styles.container}>
         <Text>Edit src/app/index.tsx to edit this screen.</Text>
-        <Button onPress={() => Alert.alert("Alert Button pressed.")} />
+        <Button onPress={() => Alert.alert("Alert Button pressed.")} title="Alert"/>
       </View>
     </>
   );
