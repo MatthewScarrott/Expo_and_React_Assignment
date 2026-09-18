73e061b1cf6f42a53cff2fa888efdaeb5c396b3d
MScarrott <scarrottmatthew@gmail.com>
2026-09-17 20:10:29 -0600

    Added images and fixed some spacing with padding

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,15 @@
-import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { View, Text, Image, StyleSheet, Button, ScrollView, Alert } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import img1 from '@/assets/images/external/img_1.jpg';
+import img2 from '@/assets/images/external/img_2.jpg';
+import img3 from '@/assets/images/external/img_3.jpg';
+import img4 from '@/assets/images/external/img_4.jpg';
+import img5 from '@/assets/images/external/img_5.jpg';
+import img6 from '@/assets/images/external/img_6.jpg';
+import img7 from '@/assets/images/external/img_7.jpg';
+import img8 from '@/assets/images/external/img_8.jpg';
+import img9 from '@/assets/images/external/img_9.jpg';
 
 export default function Index() {
   return (
@@ -18,58 +28,80 @@ export default function Index() {
           <Text>Add</Text>
         </View>
       </View>
+      <ScrollView>
+        <View style={styles.subHeader}>
+          <View style={styles.profilePhoto}>
+            <Text style={styles.profilePhotoText}>OO{'\n'}TD</Text>
+          </View>
 
-      <View style={styles.subHeader}>
-        <View style={styles.profilePhoto}>
-          <Text style={styles.profilePhotoText}>OO{'\n'}TD</Text>
+          <View style={styles.subHeaderCols}>
+            <Text>53</Text>
+            <Text>Posts</Text>
+          </View>
+
+          <View style={styles.subHeaderCols}>
+            <Text>12</Text>
+            <Text>Members</Text>
+          </View>
+
+          <View style={styles.subHeaderCols}>
+            <Text>1</Text>
+            <Text>Admins</Text>
+          </View>
         </View>
 
-        <View style={styles.subHeaderCols}>
-          <Text>53</Text>
-          <Text>Posts</Text>
+        <View style={styles.description}>
+          <Text>OOTD Everyday</Text>
+          <Text>Fit check!</Text>
+          <Text>You know we'll hype you up.</Text>
         </View>
 
-        <View style={styles.subHeaderCols}>
-          <Text>12</Text>
-          <Text>Members</Text>
+        <View style={styles.memberDropMenu}>
+          <Button onPress={() => console.log("Member dropdown pressed!")} title="Member"/>
         </View>
 
-        <View style={styles.subHeaderCols}>
-          <Text>1</Text>
-          <Text>Admins</Text>
+        <View>
+          <View style={styles.row}>
+            <Image source={img1} style={styles.image} resizeMode="center"/>
+            <Image source={img2} style={styles.image} resizeMode="center"/>
+            <Image source={img3} style={styles.image} resizeMode="center"/>
+          </View>
+          <View style={styles.row}>
+            <Image source={img4} style={styles.image} resizeMode="center"/>
+            <Image source={img5} style={styles.image} resizeMode="center"/>
+            <Image source={img6} style={styles.image} resizeMode="center"/>
+          </View>
+          <View style={styles.row}>
+            <Image source={img7} style={styles.image} resizeMode="center"/>
+            <Image source={img8} style={styles.image} resizeMode="center"/>
+            <Image source={img9} style={styles.image} resizeMode="center"/>
+          </View>
         </View>
-      </View>
 
-      <View>
-        <Text>OOTD Everyday</Text>
-        <Text>Fit check!</Text>
-        <Text>You know we'll hype you up.</Text>
-      </View>
-
-      <View style={styles.memberDropMenu}>
-        <Button onPress={() => console.log("Member dropdown pressed!")} title="Member"/>
-      </View>
-
-      <View>
-        <View style={styles.row}>
-          <Text>Image 1</Text>
-          <Text>Image 2</Text>
-          <Text>Image 3</Text>
+        <View style={styles.alert}>
+          <Button onPress={() => Alert.alert("Alert Button pressed.")} title="Alert"/>
         </View>
-        <View style={styles.row}>
-          <Text>Image 4</Text>
-          <Text>Image 5</Text>
-          <Text>Image 6</Text>
+      </ScrollView>
+      <View style={styles.footer}>
+        <View>
+          <Text>Home</Text>
         </View>
-        <View style={styles.row}>
-          <Text>Image 7</Text>
-          <Text>Image 8</Text>
-          <Text>Image 9</Text>
-        </View>
-      </View>
 
-      <View>
-        <Button onPress={() => Alert.alert("Alert Button pressed.")} title="Alert"/>
+        <View>
+          <Text>Search</Text>
+        </View>
+
+        <View>
+          <Text>Reels</Text>
+        </View>
+
+        <View>
+          <Text>Shop</Text>
+        </View>
+
+        <View>
+          <Text>Profile</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -79,7 +111,8 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    padding: 12,
   },
   headerTitle: {
     alignItems: "center"
@@ -87,7 +120,8 @@ const styles = StyleSheet.create({
   subHeader: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    padding: 12
   },
   profilePhoto: {
     width: 75,
@@ -104,13 +138,32 @@ const styles = StyleSheet.create({
   subHeaderCols: {
     alignItems: "center"
   },
+  description: {
+    padding: 12
+  },
   memberDropMenu: {
     borderWidth: 1,
-    borderColor: "#000"
+    borderColor: "#000",
+    margin: 12
   },
   row: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    padding: 12
+  },
+  image: {
+    width: 100,
+    height: 100
+  },
+  alert: {
+    padding: 12
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 32,
+    paddingRight: 32
   }
 });
